e5365f9589a173e4b8772bfe31f732ef047f325b
Luca Longinotti <luca.longinotti@inilabs.com>
2016-04-04 10:55:15 +0000

    libcaer: add simple nearest-neighbor demosaic.
    git-svn-id: https://svn.code.sf.net/p/jaer/code/libcaer/trunk@8443 b7f4320f-462c-0410-a916-d9f35bb82d52

--- src/frame_utils.c
+++ src/frame_utils.c
@@ -1,6 +1,531 @@
 #include "frame_utils.h"
 
+enum pixelColorEnum {
+	PXR, PXB, PXG1, PXG2, PXW
+};
+
 static void frameUtilsDemosaicFrame(caerFrameEvent colorFrame, caerFrameEvent monoFrame);
+
+static inline enum pixelColorEnum determinePixelColor(enum caer_frame_event_color_filter colorFilter, int32_t x,
+	int32_t y) {
+	switch (colorFilter) {
+		case RGBG:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXB);
+				}
+				else {
+					return (PXG1);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXG2);
+				}
+				else {
+					return (PXR);
+				}
+			}
+			break;
+
+		case GRGB:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXG2);
+				}
+				else {
+					return (PXR);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXB);
+				}
+				else {
+					return (PXG1);
+				}
+			}
+			break;
+
+		case GBGR:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXG1);
+				}
+				else {
+					return (PXB);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXR);
+				}
+				else {
+					return (PXG2);
+				}
+			}
+			break;
+
+		case BGRG:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXR);
+				}
+				else {
+					return (PXG2);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXG1);
+				}
+				else {
+					return (PXB);
+				}
+			}
+			break;
+
+		case RGBW:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXB);
+				}
+				else {
+					return (PXG1);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXW);
+				}
+				else {
+					return (PXR);
+				}
+			}
+			break;
+
+		case GRWB:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXW);
+				}
+				else {
+					return (PXR);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXB);
+				}
+				else {
+					return (PXG1);
+				}
+			}
+			break;
+
+		case WBGR:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXG1);
+				}
+				else {
+					return (PXB);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXR);
+				}
+				else {
+					return (PXW);
+				}
+			}
+			break;
+
+		case BWRG:
+			if (x & 0x01) {
+				if (y & 0x01) {
+					return (PXR);
+				}
+				else {
+					return (PXW);
+				}
+			}
+			else {
+				if (y & 0x01) {
+					return (PXG1);
+				}
+				else {
+					return (PXB);
+				}
+			}
+			break;
+
+		default:
+			// Just fall through.
+			break;
+	}
+
+	// This is impossible (MONO), so just use red pixel value, as good as any.
+	return (PXR);
+}
+
+static void frameUtilsDemosaicFrame(caerFrameEvent colorFrame, caerFrameEvent monoFrame) {
+	uint16_t *colorPixels = caerFrameEventGetPixelArrayUnsafe(colorFrame);
+	uint16_t *monoPixels = caerFrameEventGetPixelArrayUnsafe(monoFrame);
+
+	enum caer_frame_event_color_filter colorFilter = caerFrameEventGetColorFilter(monoFrame);
+	int32_t lengthY = caerFrameEventGetLengthY(monoFrame);
+	int32_t lengthX = caerFrameEventGetLengthX(monoFrame);
+	int32_t idxCENTER = 0;
+	int32_t idxCOLOR = 0;
+
+	for (int32_t y = 0; y < lengthY; y++) {
+		for (int32_t x = 0; x < lengthX; x++) {
+			// Calculate all neighbor indexes.
+			int32_t idxLEFT = idxCENTER - 1;
+			int32_t idxRIGHT = idxCENTER + 1;
+
+			int32_t idxCENTERUP = idxCENTER - lengthX;
+			int32_t idxLEFTUP = idxCENTERUP - 1;
+			int32_t idxRIGHTUP = idxCENTERUP + 1;
+
+			int32_t idxCENTERDOWN = idxCENTER + lengthX;
+			int32_t idxLEFTDOWN = idxCENTERDOWN - 1;
+			int32_t idxRIGHTDOWN = idxCENTERDOWN + 1;
+
+			enum pixelColorEnum pixelColor = determinePixelColor(colorFilter, x, y);
+			int32_t RComp;
+			int32_t GComp;
+			int32_t BComp;
+
+			switch (pixelColor) {
+				case PXR: {
+					// This is a R pixel. It is always surrounded by G and B only.
+					RComp = monoPixels[idxCENTER];
+
+					if (y == 0) {
+						// First row.
+						if (x == 0) {
+							// First column.
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxRIGHT]) / 2;
+							BComp = monoPixels[idxRIGHTDOWN];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]) / 2;
+							BComp = monoPixels[idxLEFTDOWN];
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 3;
+							BComp = (monoPixels[idxRIGHTDOWN] + monoPixels[idxLEFTDOWN]) / 2;
+						}
+					}
+					else if (y == (lengthY - 1)) {
+						// Last row.
+						if (x == 0) {
+							// First column.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxRIGHT]) / 2;
+							BComp = monoPixels[idxRIGHTUP];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxLEFT]) / 2;
+							BComp = monoPixels[idxLEFTUP];
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 3;
+							BComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP]) / 2;
+						}
+					}
+					else {
+						// In-between rows.
+						if (x == 0) {
+							// First column.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxRIGHT]) / 3;
+							BComp = (monoPixels[idxRIGHTUP] + monoPixels[idxRIGHTDOWN]) / 2;
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]) / 3;
+							BComp = (monoPixels[idxLEFTUP] + monoPixels[idxLEFTDOWN]) / 2;
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]
+								+ monoPixels[idxRIGHT]) / 4;
+							BComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP] + monoPixels[idxRIGHTDOWN]
+								+ monoPixels[idxLEFTDOWN]) / 4;
+						}
+					}
+
+					break;
+				}
+
+				case PXB: {
+					// This is a B pixel. It is always surrounded by G and R only.
+					BComp = monoPixels[idxCENTER];
+
+					if (y == 0) {
+						// First row.
+						if (x == 0) {
+							// First column.
+							RComp = monoPixels[idxRIGHTDOWN];
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxRIGHT]) / 2;
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = monoPixels[idxLEFTDOWN];
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]) / 2;
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxRIGHTDOWN] + monoPixels[idxLEFTDOWN]) / 2;
+							GComp = (monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 3;
+						}
+					}
+					else if (y == (lengthY - 1)) {
+						// Last row.
+						if (x == 0) {
+							// First column.
+							RComp = monoPixels[idxRIGHTUP];
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxRIGHT]) / 2;
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = monoPixels[idxLEFTUP];
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxLEFT]) / 2;
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP]) / 2;
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 3;
+						}
+					}
+					else {
+						// In-between rows.
+						if (x == 0) {
+							// First column.
+							RComp = (monoPixels[idxRIGHTUP] + monoPixels[idxRIGHTDOWN]) / 2;
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxRIGHT]) / 3;
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = (monoPixels[idxLEFTUP] + monoPixels[idxLEFTDOWN]) / 2;
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]) / 3;
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP] + monoPixels[idxRIGHTDOWN]
+								+ monoPixels[idxLEFTDOWN]) / 4;
+							GComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN] + monoPixels[idxLEFT]
+								+ monoPixels[idxRIGHT]) / 4;
+						}
+					}
+
+					break;
+				}
+
+				case PXG1: {
+					// This is a G1 (first green) pixel. It is always surrounded by all of R, G, B.
+					GComp = monoPixels[idxCENTER];
+
+					if (y == 0) {
+						// First row.
+						BComp = monoPixels[idxCENTERDOWN];
+
+						if (x == 0) {
+							// First column.
+							RComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else if (y == (lengthY - 1)) {
+						// Last row.
+						BComp = monoPixels[idxCENTERUP];
+
+						if (x == 0) {
+							// First column.
+							RComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else {
+						// In-between rows.
+						BComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN]) / 2;
+
+						if (x == 0) {
+							// First column.
+							RComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							RComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							RComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+
+					break;
+				}
+
+				case PXG2: {
+					// This is a G2 (second green) pixel. It is always surrounded by all of R, G, B.
+					GComp = monoPixels[idxCENTER];
+
+					if (y == 0) {
+						// First row.
+						RComp = monoPixels[idxCENTERDOWN];
+
+						if (x == 0) {
+							// First column.
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							BComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else if (y == (lengthY - 1)) {
+						// Last row.
+						RComp = monoPixels[idxCENTERUP];
+
+						if (x == 0) {
+							// First column.
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							BComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else {
+						// In-between rows.
+						RComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN]) / 2;
+
+						if (x == 0) {
+							// First column.
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							BComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+
+					break;
+				}
+
+				case PXW: {
+					// This is a W pixel, modified Bayer pattern instead of G2. It is always surrounded by all of R, G, B.
+					// TODO: how can W itself contribute to the three colors?
+					if (y == 0) {
+						// First row.
+						RComp = monoPixels[idxCENTERDOWN];
+
+						if (x == 0) {
+							// First column.
+							GComp = monoPixels[idxRIGHTDOWN];
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = monoPixels[idxLEFTDOWN];
+							BComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxRIGHTDOWN] + monoPixels[idxLEFTDOWN]) / 2;
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else if (y == (lengthY - 1)) {
+						// Last row.
+						RComp = monoPixels[idxCENTERUP];
+
+						if (x == 0) {
+							// First column.
+							GComp = monoPixels[idxRIGHTUP];
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = monoPixels[idxLEFTUP];
+							BComp = monoPixels[idxRIGHT];
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP]) / 2;
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+					else {
+						// In-between rows.
+						RComp = (monoPixels[idxCENTERUP] + monoPixels[idxCENTERDOWN]) / 2;
+
+						if (x == 0) {
+							// First column.
+							GComp = (monoPixels[idxRIGHTUP] + monoPixels[idxRIGHTDOWN]) / 2;
+							BComp = monoPixels[idxRIGHT];
+						}
+						else if (x == (lengthX - 1)) {
+							// Last column.
+							GComp = (monoPixels[idxLEFTUP] + monoPixels[idxLEFTDOWN]) / 2;
+							BComp = monoPixels[idxLEFT];
+						}
+						else {
+							// In-between columns.
+							GComp = (monoPixels[idxRIGHTUP] + monoPixels[idxLEFTUP] + monoPixels[idxRIGHTDOWN]
+								+ monoPixels[idxLEFTDOWN]) / 4;
+							BComp = (monoPixels[idxLEFT] + monoPixels[idxRIGHT]) / 2;
+						}
+					}
+
+					break;
+				}
+			}
+
+			// Set color frame pixel values for all color channels.
+			colorPixels[idxCOLOR] = U16T(RComp);
+			colorPixels[idxCOLOR + 1] = U16T(GComp);
+			colorPixels[idxCOLOR + 2] = U16T(BComp);
+
+			// Go to next pixel.
+			idxCENTER++;
+			idxCOLOR += RGB;
+		}
+	}
+}
 
 caerFrameEventPacket caerFrameUtilsDemosaic(caerFrameEventPacket framePacket) {
 	int32_t countValid = 0;
@@ -62,10 +587,6 @@ caerFrameEventPacket caerFrameUtilsDemosaic(caerFrameEventPacket framePacket) {
 	CAER_FRAME_ITERATOR_ALL_END
 
 	return (colorFramePacket);
-}
-
-static void frameUtilsDemosaicFrame(caerFrameEvent colorFrame, caerFrameEvent monoFrame) {
-
 }
 
 void caerFrameUtilsAutoContrastBrigthness(caerFrameEventPacket framePacket) {
